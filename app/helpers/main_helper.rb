81d8fb7be761785b25bfeb4b04298332731474a4
Matijs van Zuijlen <matijs@matijs.net>
2009-03-11 23:14:34 +0100

    Use precalculated status code in helper methods

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -3,20 +3,13 @@ module MainHelper
   STATUS_TEXTS = ["Nee", "", "Ja", "Beste"] # "Misschien" for "" ?
 
   def status_to_class(status, max, numplayers)
-    return STATUS_CLASSES[status_code(status, max, numplayers)]
+    return STATUS_CLASSES[status[:code]]
   end
 
   def status_to_text(status, max, numplayers)
-    return STATUS_TEXTS[status_code(status, max, numplayers)]
+    return STATUS_TEXTS[status[:code]]
   end
 
-  def status_code(status, max, numplayers)
-    min = [numplayers, MainController::MIN_PLAYERS].min
-    return 3 if max >= min && status[:yes] == max
-    return 2 if status[:yes] >= min
-    return 0 if status[:no] > (numplayers - min)
-    return 1
-  end
 
   def class_if_current_user(player)
     player == @current_user ? "current" : ""
