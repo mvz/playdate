8ac3006ac9163f0371df78d3d676eb61eacb4fe6
Matijs van Zuijlen <matijs@matijs.net>
2009-09-11 23:29:08 +0200

    Move nl error messages to correct new location.

--- config/environment.rb
+++ config/environment.rb
@@ -58,18 +58,4 @@ Rails::Initializer.run do |config|
   # config.active_record.default_timezone = :utc
 end
 
-# Nederlandse error messages
-ActiveRecord::Errors.default_error_messages[:inclusion] = " is niet opgenomen in de lijst" 
-ActiveRecord::Errors.default_error_messages[:exclusion] = " is niet toegestaan in de lijst" 
-ActiveRecord::Errors.default_error_messages[:invalid] = " is ongeldig" 
-ActiveRecord::Errors.default_error_messages[:confirmation] = " is niet gelijk aan de bevestiging" 
-ActiveRecord::Errors.default_error_messages[:accepted] = " moet geaccepteerd worden" 
-ActiveRecord::Errors.default_error_messages[:empty] = " mag niet leeg zijn" 
-ActiveRecord::Errors.default_error_messages[:blank] = " mag niet leeg zijn" 
-ActiveRecord::Errors.default_error_messages[:too_long] = " is te lang (maximum is %d tekens)" 
-ActiveRecord::Errors.default_error_messages[:too_short] = " is te kort (minimum is %d tekens)" 
-ActiveRecord::Errors.default_error_messages[:wrong_length] = " heeft de verkeerde lengte (moet %d tekens zijn)" 
-ActiveRecord::Errors.default_error_messages[:taken] = " is al in gebruik" 
-ActiveRecord::Errors.default_error_messages[:not_a_number] = " is geen getal" 
-
 require 'will_paginate'
