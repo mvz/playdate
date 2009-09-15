ffc754baad31cf61a6c2b1dd7771446e2c6c1ede
Matijs van Zuijlen <matijs@matijs.net>
2009-09-15 20:28:09 +0200

    work on player model.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,13 @@
 require 'ajax_scaffold'
+require 'digest/sha2'
 
 class Player < ActiveRecord::Base
   SALT = 'change this to your own salt'
+
+  @scaffold_columns = %w(name full_name abbreviation is_admin).map {|c|
+    AjaxScaffold::ScaffoldColumn.new(self, {:name => c})
+  }
+
   def password=(pass)
     self.password_hash = Digest::SHA256.hexdigest(pass + SALT)
   end
