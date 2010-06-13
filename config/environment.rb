a1fb950fca347e2074d12b9942c69a02295035cb
Matijs van Zuijlen <matijs@matijs.net>
2010-06-13 10:34:48 +0200

    Don't need to use github anymore for will_paginate gem.

--- config/environment.rb
+++ config/environment.rb
@@ -15,8 +15,7 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem 'mislav-will_paginate', :version => '~> 2.3.11', :lib => 'will_paginate', 
-          :source => 'http://gems.github.com'
+  config.gem 'will_paginate', :version => '~> 2.3.11'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
