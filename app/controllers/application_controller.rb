9c11ed23f67f0d55aa0cda53e706b4ae7c0e2753
Matijs van Zuijlen <matijs@matijs.net>
2010-08-19 00:22:51 +0200

    Refactor authorize and authorize_admin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,26 +9,27 @@ class ApplicationController < ActionController::Base
   # filter_parameter_logging :password
 
   private
+
   def authorize
     @current_user = current_user
     @current_user or access_denied_not_logged_in
   end
+
   def authorize_admin 
-    @current_user = current_user
-    unless @current_user
-      access_denied_not_logged_in
-    else
-      @current_user.is_admin or access_denied_no_admin
-    end
+    authorize or return false
+    @current_user.is_admin or access_denied_no_admin
   end
+
   def current_user
     Player.find_by_id(session[:user_id])
   end
+
   def access_denied_not_logged_in
     flash[:notice] = "Log eerst in om Playdate te gebruiken."
     redirect_to(:controller => "login", :action => "login")
     return false
   end
+
   def access_denied_no_admin
     flash[:notice] = "Toegang geweigerd: Je bent geen beheerder."
     redirect_to(:controller => "main", :action => "index")
