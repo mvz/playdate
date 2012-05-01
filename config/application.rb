c4296f750a9b61a99e16225ae9b5bb764b4c2cae
Matijs van Zuijlen <matijs@matijs.net>
2012-05-01 12:21:28 +0200

    Enable asset pipeline.

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,9 @@ module PlayDate
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
     config.active_record.timestamped_migrations = false
   end
 end
