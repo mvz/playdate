1b1b6692238444477355f94037ca5c353464c3c2
Matijs van Zuijlen <matijs@matijs.net>
2013-07-26 12:38:37 +0200

    Rewrite PlayersController tests with describe blocks

--- test/controllers/players_controller_test.rb
+++ test/controllers/players_controller_test.rb
@@ -1,121 +1,129 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class PlayersControllerTest < ActionController::TestCase
-  NEW_PLAYER = {:name => 'Testy', :password => 'test123', :password_confirmation => 'test123'}
-  REDIRECT_TO_MAIN = {:controller => 'players', :action => 'index'}
+describe PlayersController do
+  let(:adminsession) { { :user_id => players(:admin).id } }
 
-  def setup
-    @adminsession = {:user_id => players(:admin).id }
-  end
-
-  def test_authorization
-    playersession = {:user_id => players(:matijs).id }
-    [
-      [{}, "login", "login"],
-      [playersession, "main", "index"]
-    ].each do |session,controller,action|
-      [:create, :destroy, :update].each do |a|
-        [:get, :post].each do |m|
-          [ lambda { method(m).call(a, {:id => 1}, session) } ].each do |e|
-            e.call
-            assert_redirected_to :controller => controller,
-              :action => action
-          end
+  describe 'when not logged in' do
+    [:create, :destroy, :update].each do |a|
+      [:get, :post].each do |m|
+        it "requires login for #{m} #{a}" do
+          send m, a, id: 1
+          assert_redirected_to login_path
         end
       end
     end
   end
 
-  def test_index
-    get :index, {}, @adminsession
+  describe 'when logged in as a regular player' do
+    let(:playersession) { { :user_id => players(:matijs).id } }
 
-    assert_template 'index'
-    assert_select "h1", "Spelers"
-  end
-
-  def test_edit
-    get :edit, {:id => 1}, @adminsession
-
-    assert_template 'edit'
-    assert_select "h1", "Speler bewerken"
-  end
-
-  def test_new
-    get :new, {}, @adminsession
-
-    assert_template 'new'
-    assert_select "h1", "Nieuwe speler"
-  end
-
-  def test_create
-    player_count = Player.all.length
-    post :create, {:player => NEW_PLAYER}, @adminsession
-    check_attrs(%w(player))
-    assert_response :redirect
-    assert_redirected_to REDIRECT_TO_MAIN
-    assert_equal player_count + 1, Player.all.length, "Expected an additional Player"
-  end
-
-  def test_update
-    @first = Player.first
-    player_count = Player.all.length
-    post :update,
-      {:id => @first.id, :player => NEW_PLAYER},
-      @adminsession
-    player = check_attrs(%w(player))
-    player.reload
-    NEW_PLAYER.each do |attr_name|
-      assert_equal NEW_PLAYER[attr_name], player.attributes[attr_name], "@player.#{attr_name.to_s} incorrect"
+    [:create, :destroy, :update].each do |a|
+      [:get, :post].each do |m|
+        it "denies access for #{m} #{a}" do
+          send m, a, { id: 1 }, playersession
+          assert_redirected_to root_path
+        end
+      end
     end
-    assert_equal player_count, Player.all.length, "Number of Players should be the same"
-    assert_response :redirect
-    assert_redirected_to REDIRECT_TO_MAIN
   end
 
-  def test_destroy
-    player_count = Player.all.length
-
-    player = players(:matijs)
-    num_avs = Availability.count
-    num_player_avs = player.availabilities.count
-    assert num_player_avs > 0, "Test won't work if pd has no availabilities"
-
-    post :destroy, {:id => players(:matijs).id}, @adminsession
-    assert_response :redirect
-    assert_equal player_count - 1, Player.all.length, "Number of Players should be one less"
-    assert_redirected_to REDIRECT_TO_MAIN
-
-    assert_equal num_avs - num_player_avs, Availability.count
-  end
-
-  def test_cannot_destroy_self
-    player_count = Player.all.length
-    post :destroy, {:id => players(:admin).id}, @adminsession
-    assert_equal player_count, Player.all.length, "Number of Players should stay the same"
-    assert_response :redirect
-    assert_redirected_to REDIRECT_TO_MAIN
-  end
-
-  protected
-
-  # Could be put in a Helper library and included at top of test class
-  def check_attrs(attr_list)
-    attrs = []
-    attr_list.each do |attr_sym|
-      attr = assigns(attr_sym.to_sym)
-      assert_not_nil attr,       "Attribute @#{attr_sym} should not be nil"
-      assert !attr.new_record?,  "Should have saved the @#{attr_sym} obj" if attr.class == ActiveRecord
-      attrs << attr
+  describe "#index" do
+    before do
+      get :index, {}, adminsession
     end
-    attrs.length > 1 ? attrs : attrs[0]
+
+    it 'renders index' do
+      assert_template 'index'
+    end
+
+    it 'has the correct heading' do
+      assert_select "h1", "Spelers"
+    end
   end
-end
 
-describe PlayersController do
-  let(:adminsession) { { :user_id => players(:admin).id } }
+  describe "#edit" do
+    before do
+      get :edit, {:id => 1}, adminsession
+    end
 
-  before do
-    adminsession
+    it 'renders edit' do
+      assert_template 'edit'
+    end
+
+    it 'has the correct heading' do
+      assert_select "h1", "Speler bewerken"
+    end
+  end
+
+  describe "#new" do
+    before do
+      get :new, {}, adminsession
+    end
+
+    it 'renders new' do
+      assert_template 'new'
+    end
+
+    it 'has the correct heading' do
+      assert_select "h1", "Nieuwe speler"
+    end
+  end
+
+  describe "#create" do
+    let(:player_params) { { name: 'new',
+                            full_name: 'New Name',
+                            abbreviation: 'nn',
+                            password: 'test123',
+                            password_confirmation: 'test123' } }
+
+    before do
+      @player_count = Player.all.length
+      post :create, {:player => player_params}, adminsession
+    end
+
+    it 'assigns to @player' do
+      assigns(:player).wont_be_nil
+    end
+
+    it 'redirects to the player list' do
+      assert_redirected_to players_path
+    end
+
+    it 'increases the number of players' do
+      assert_equal @player_count + 1, Player.all.length
+    end
+  end
+
+  describe "#destroy" do
+    let(:player) { players(:matijs) }
+
+    before do
+      @player_count = Player.all.length
+      @num_avs = Availability.count
+      @num_player_avs = player.availabilities.count
+      post :destroy, {:id => player.id}, adminsession
+    end
+
+    it 'redirects to the player list' do
+      assert_redirected_to players_path
+    end
+
+    it 'decreases the number of players' do
+      assert_equal @player_count - 1, Player.all.length
+    end
+
+    it "destroys the player's availabilities" do
+      assert @num_player_avs > 0
+      assert_equal @num_avs - @num_player_avs, Availability.count
+    end
+
+    describe 'when attempting to destroy onesself' do
+      let(:player) { players(:admin) }
+
+      it 'does not reduce the number of players' do
+        assert_equal @player_count, Player.all.length
+      end
+    end
   end
 
   describe "#update" do
@@ -124,12 +132,20 @@ describe PlayersController do
                             abbreviation: 'nn',
                             is_admin: true,
                             default_status: Availability::STATUS_JA.to_s } }
+
     it 'updates all desired attributes' do
+      adminsession
       player = MiniTest::Mock.new
       player.expect(:update_attributes, true, [player_params.with_indifferent_access])
       Player.stub :find, player do
         post :update, {id: 1, player: player_params}, adminsession
       end
+      player.verify
+    end
+
+    it 'redirects to the player list' do
+      post :update, {id: 1, player: player_params}, adminsession
+      assert_redirected_to players_path
     end
   end
 end
