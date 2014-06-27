6268b93b0727d0cd7d9f11b7c4652f03dca800ee
Matijs van Zuijlen <matijs@matijs.net>
2014-06-27 15:48:06 +0200

    Rely on test_helper to be in the load path

--- test/controllers/players_controller_test.rb
+++ test/controllers/players_controller_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require 'test_helper'
 
 describe PlayersController do
   let(:adminsession) { { :user_id => players(:admin).id } }
--- test/models/playdate_test.rb
+++ test/models/playdate_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require 'test_helper'
 
 class PlaydateTest < ActiveSupport::TestCase
   REQ_ATTR_NAMES = %w(day)
