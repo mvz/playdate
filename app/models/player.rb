446afdc1863d82871b8d13b30762c7103f245b9a
Matijs van Zuijlen <matijs@matijs.net>
2016-08-22 10:16:10 +0100

    Prefetch playdates to avoid extra DB access

--- app/models/player.rb
+++ app/models/player.rb
@@ -39,7 +39,7 @@ class Player < ActiveRecord::Base
 
   def availabilities_by_day
     # TODO: Deprecated?
-    availabilities.each_with_object({}) { |av, h| h[av.playdate.day] = av }
+    availabilities.includes(:playdate).each_with_object({}) { |av, h| h[av.playdate.day] = av }
   end
 
   def availability_for_playdate(pd)
