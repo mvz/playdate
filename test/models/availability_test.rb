7a8615129c83527a0e8c6f16b106e3d60ae7cb27
Matijs van Zuijlen <matijs@matijs.net>
2018-05-28 16:00:00 +0200

    Fix Rails/AssertNot offense

--- test/models/availability_test.rb
+++ test/models/availability_test.rb
@@ -7,7 +7,7 @@ class AvailabilityTest < ActiveSupport::TestCase
 
   def test_raw_validation
     availability = Availability.new
-    assert !availability.valid?
+    assert_not availability.valid?
     REQ_ATTR_NAMES.each { |attr_name|
       assert availability.errors[attr_name.to_sym].any?,
         "Should be an error message for :#{attr_name}"
