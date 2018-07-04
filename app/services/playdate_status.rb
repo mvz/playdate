1c03c7350fb20336cc2a27a76fccb90064419c69
Matijs van Zuijlen <matijs@matijs.net>
2018-07-04 14:06:11 +0200

    Extract methods

--- app/services/playdate_status.rb
+++ app/services/playdate_status.rb
@@ -2,14 +2,19 @@
 
 class PlaydateStatus
   def self.calculate(playdates, players)
-    new.statistics(playdates, players)
+    new(playdates, players).statistics
   end
 
-  def statistics(playdates, players)
-    availabilities = Availability.where(playdate_id: playdates, player_id: players)
+  attr_reader :playdates, :players
 
+  def initialize(playdates, players)
+    @playdates = playdates
+    @players = players
+  end
+
+  def statistics
     stats = playdates.each_with_object({}) do |pd, h|
-      date_avs = availabilities.select { |it| it.playdate_id == pd.id }
+      date_avs = relevant_availabilities.select { |it| it.playdate_id == pd.id }
 
       stat = Hash.new(0)
       players.each do |p|
@@ -29,15 +34,21 @@ class PlaydateStatus
     max_has_house = !stats.find { |_d, s|
       s[:yes] == max && s[:house] > 0
     }.nil?
-    numplayers = players.length
-    min = [numplayers, MainController::MIN_PLAYERS].min
 
     stats.each_value do |s|
-      s[:code] = status_code(s, min, max, max_has_house, numplayers)
+      s[:code] = status_code(s, max, max_has_house, numplayers)
     end
   end
 
-  def status_code(status, min, max, max_has_house, numplayers)
+  private
+
+  def relevant_availabilities
+    @relevant_availabilities ||=
+      Availability.where(playdate_id: playdates, player_id: players)
+  end
+
+  def status_code(status, max, max_has_house, numplayers)
+    min = minimum_players_needed
     if max >= min && status[:yes] == max
       return 3 unless max_has_house
       return 3 if status[:house] > 0
@@ -46,5 +57,13 @@ class PlaydateStatus
     return 2 if status[:yes] >= min
     return 0 if status[:no] > (numplayers - min)
     1
+  end
+
+  def minimum_players_needed
+    [numplayers, MainController::MIN_PLAYERS].min
+  end
+
+  def numplayers
+    players.length
   end
 end
