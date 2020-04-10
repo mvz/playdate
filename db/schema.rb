24a2c644b4d86e36ab52b26d356d3753c3dcf52e
Matijs van Zuijlen <matijs@matijs.net>
2020-04-10 15:25:02 +0200

    Add unique indexes to back up uniqueness validations

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 7) do
+ActiveRecord::Schema.define(version: 8) do
 
   create_table "availabilities", force: :cascade do |t|
     t.integer "player_id"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 7) do
     t.datetime "updated_at"
     t.datetime "created_at"
     t.index ["playdate_id"], name: "index_availabilities_on_playdate_id"
+    t.index ["player_id", "playdate_id"], name: "index_availabilities_on_player_id_and_playdate_id", unique: true
     t.index ["player_id"], name: "index_availabilities_on_player_id"
   end
 
@@ -26,6 +27,7 @@ ActiveRecord::Schema.define(version: 7) do
     t.date "day"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["day"], name: "index_playdates_on_day", unique: true
   end
 
   create_table "players", force: :cascade do |t|
@@ -38,6 +40,7 @@ ActiveRecord::Schema.define(version: 7) do
     t.integer "default_status"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["name"], name: "index_players_on_name", unique: true
   end
 
 end
