355189cc874d3b123efac314991fce963eb78c78
Matijs van Zuijlen <matijs@matijs.net>
2023-11-19 20:55:11 +0100

    Update application using the rails app:update task

--- config/application.rb
+++ config/application.rb
@@ -22,11 +22,13 @@ Bundler.require(*Rails.groups)
 
 module PlayDate
   class Application < Rails::Application
-    # Use the responders controller from the responders gem
-    config.app_generators.scaffold_controller :responders_controller
-
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -36,15 +38,7 @@ module PlayDate
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.generators do |g|
-      g.test_framework :rspec
-      g.system_tests nil
-      g.integration_tool nil
-    end
-
-    # Since this is an old application, it uses numbered migrations.
-    config.active_record.timestamped_migrations = false
-
-    config.i18n.enforce_available_locales = true
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
